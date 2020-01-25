72a1f0483b0d57f65410e8baaf503a92e1d80104
Mantautas <Mantautas.rimkus@gmail.com>
2020-01-24 22:14:33 -0700

    Summary calculations, prepared for a modelling

--- scripts/Mantautas_DataReading.R
+++ scripts/Mantautas_DataReading.R
@@ -6,92 +6,122 @@
 ############# Setup ##############
 ##################################
 
-library(ggplot2)
-library(dplyr)
-library(magrittr)
+#####################################
 
-#####################################
-############# Main Code #############
-#####################################
 #Libraries
 library("lubridate")
 library("dplyr")
+library("plyr")
+library(readr)
 
-#Data reading - 
+#####################################
+############# Main Code #############
+
+#DATA READING------------------------------ 
+
 #minbymin_raw_list is a list of minbymin. To approach person i data, just write minbymin_raw_list[[i]].
 #exploratory_data is a data about persons.
 
-files_names <- list.files("raw_data")
-activities_files <- files_names[grepl("minbymin",files_names)]
+  #Reading names of files in folder raw_data
+  files_names <- list.files("raw_data")
+  activities_files <- files_names[grepl("minbymin",files_names)]
+  
+  #Reading data frame into list format. 
+  minbymin_raw_list <- vector(mode = "list", length = length(activities_files))
+  names(minbymin_raw_list) <- activities_files
 
-minbymin_raw_list <- vector(mode = "list", length = length(activities_files))
+  for(i in 1:length(activities_files)){
+    minbymin_raw_list[[i]] <- read.csv(file = paste0("raw_data/",activities_files[i]))
+  }
+  
+  #Person id.22 has strange first line, so we just dropped it. 
+  minbymin_raw_list[[21]] <- minbymin_raw_list[[21]][-1,]
+  
+  #Read data about persons
+  exploratory_data <- read.csv(file="raw_data/demographics.csv")
 
-for(i in 1:length(activities_files)){
-  minbymin_raw_list[[i]] <- read.csv(file = paste0("raw_data/",activities_files[i]))
-}
 
-exploratory_data <- read.csv(file="raw_data/demographics.csv")
-
+  
 #SET-UP PARAMETERS
-COUNT_AS_HOUR_LIMIT <- 18
+  
+  COUNT_AS_HOUR_LIMIT <- 18 #This one sets where our day starts. 
+  CONSIDERED_AS_SLEEPING <- 10 #This one sets how many minutes person need to rest to be consedered as resting
 
 
-
-
-
-llls <- lapply(minbymin_raw_list, function(xframe){
-  dates <- dmy_hms(xframe$time_new)
+#SUMMARY STATISTICS CALCULATOR.
+  #For each person, this codes slices his measurements into bin by fake days, where day end id defined by variable
+  #COUNT_AS_HOUR_LIMIT. This code return data frame, where we have variables:
+    #.id = persons id
+    #Days_counter = which fake day
+    #Captured_Minutes = How many minutes of data we have for that fake day for person .id
+    #How_Many_Time_Resting = Time events, where person was resting more or equal than 10 minutes.
+    # Where must be a activity encounter to count as separate event.
+    #How_Long_Resting = Total minutes, where person was resting more than 10 minutes in a row.
+    #Total_Not_Sleeping = Total minutes, where persons met was more than 1.25
+    #Total_Active = Total minutes, where persons "mets" was more than 1.25
+    #Total_3_over = Total minutes, where persons "mets" was more or equal than 3
+    #Total_6_over = Total minutes, where persons "mets" was more or equal than 6
+    #First_Encountered_Moment = Moment where day started. 
+  
+summary_statistics_fake_days <- ldply(minbymin_raw_list, function(xframe){
+  
+  dates <- dmy_hms(xframe$time_new) #Convert to readable time
+  
+  #This part gives label for each day, to which "fake day" it gonna be included
+  
   dates_frame_x <- array(NA, dim=c(length(xframe$time_new), 4)) %>%
     set_colnames(c("Month", "Day", "Hour", "Minute")) %>%
     as_tibble %>%
     dplyr::mutate(Month=month(dates), Day=day(dates), Hour=hour(dates), Minute=minute(dates)) %>%
     dplyr::mutate(new_Month=month(dates-hours(COUNT_AS_HOUR_LIMIT)), new_Day=day(dates-hours(COUNT_AS_HOUR_LIMIT)), new_Hour=hour(dates-hours(COUNT_AS_HOUR_LIMIT)), new_Minute=minute(dates-hours(COUNT_AS_HOUR_LIMIT)))
   
-  mutated <- dates_frame_x %>% dplyr::select(new_Month, new_Day) %>% unique()
-  ffz <- mutated %>% dplyr::mutate(Days_counter=1:length(mutated$new_Month)) %>% right_join(dates_frame_x,by=c("new_Month", "new_Day")) %>%
+  date_frames_x_unique_labels <- dates_frame_x %>% dplyr::select(new_Month, new_Day) %>% unique()
+  
+  date_frame_x_with_labels <- date_frames_x_unique_labels %>% 
+    dplyr::mutate(Days_counter=1:length(date_frames_x_unique_labels$new_Month)) %>% 
+    right_join(dates_frame_x,by=c("new_Month", "new_Day")) %>%
     dplyr::select(Month, Day, Hour, Minute, Days_counter) %>%
     cbind(xframe) %>%
-    dplyr::select(everything(), -time_new)
+    dplyr::mutate(time_new=dates)
   
-  ddply(ffz, ~Days_counter, function(xxframe){
+  #Goes through each label (fake days) and calculated summary statistic
+  
+  summary_dataframe_x <- ddply(date_frame_x_with_labels, ~Days_counter, function(xxframe){
+    colnames_summary_stats <- c("Captured_Minutes", "How_Many_Time_Resting", "How_Long_Resting", "Total_Active", "Total_3_Or_Over","Total_6_Or_Over")
+    
     number_of_minutes <- dim(xxframe)[1]
-    counter_mets_over_3 <- xxframe %>% dplyr::filter(minute_mets>=3) %>% summarize(n())
-    counter_mets_over_6 <- xxframe %>% dplyr::filter(minute_mets>=6) %>% summarize(n())
-    counter_mets_over_not_minimum <- xxframe %>% dplyr::filter(minute_mets>1.25) %>% summarize(n())
+    counter_mets_over_3 <- xxframe %>%  dplyr::filter(minute_mets>=3) %>% dplyr::select(minute_mets) %>% as.matrix() %>% length()
+    counter_mets_over_6 <- xxframe %>% dplyr::filter(minute_mets>=6) %>% dplyr::select(minute_mets) %>% as.matrix() %>% length()
+    counter_mets_over_not_minimum <- xxframe %>% dplyr::filter(minute_mets>1.25) %>% dplyr::select(minute_mets) %>% as.matrix() %>% length()
     
+    sleeping_prepare <- xxframe %>% 
+      dplyr::filter(minute_mets==1.25, ap_posture==0) %>%
+      mutate(Diff=c(1,diff(time_new))-1) %>%
+      dplyr::select(Diff) %>%
+      cumsum() %>% 
+      table()
     
-    
-    
-    
-    
-    
+    sleeping <- c(length(which(sleeping_prepare >=CONSIDERED_AS_SLEEPING)), sum(sleeping_prepare[which(sleeping_prepare >=CONSIDERED_AS_SLEEPING)]))
+  
+    returning_vectors <- c(number_of_minutes, sleeping[1], sleeping[2],counter_mets_over_not_minimum, counter_mets_over_3, counter_mets_over_6)
+    names(returning_vectors) <- colnames_summary_stats
+    return(returning_vectors)
   })
   
+  #Just add timestamp of first minute included into "fake day". Gonna helps to maneuvre with data
+  summary_dataframe_x[,"First_encountered_moment"] <- (date_frame_x_with_labels %>% 
+    dplyr::group_by(Days_counter) %>%
+    dplyr::summarize(xx = min(time_new)) %>%
+    ungroup())$xx
   
+  return(summary_dataframe_x)
   
-  
-  
-  
-  
-  counter <- 0
-  
-  
-  
-  
-  
-  minipulated_dates_frame_x <- dates_frame_x %>%
-    
-                         
-                         
-                         
-                         
+},.progress="text") %>%
+  dplyr::rename(file=.id)
 
-  })
-  
+summary_statistics_fake_days <- summary_statistics_fake_days %>% mutate(id=parse_number(file))
 
-  
-  
-})
+
 
 
 
