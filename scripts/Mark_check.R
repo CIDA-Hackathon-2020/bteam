d375e19e2a1037756773eca8e78b7ea3e73f4744
Mark Gardner <markag@rams.colostate.edu>
2020-01-24 18:05:10 -0700

    fgdhxf

---
+++ scripts/Mark_check.R
@@ -0,0 +1,16 @@
+# Script Purpose:
+library(ggplot2)
+firstFile <- read.csv("minbymin_01.csv")
+
+jgjkgjgjgjk
+##################################
+############# Setup ##############
+##################################
+
+# library(ggplot2)  # for example
+
+read.csv(file.path("raw_data",  "minbymin_02.csv"))
+
+#####################################
+############# Main Code #############
+#####################################
